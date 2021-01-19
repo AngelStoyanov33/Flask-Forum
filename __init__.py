3fac1b1e4ef08924b6673234458cf21deb9711a9
Denis Zahariev <denis.zaharievv@gmail.com>
2021-01-19 21:06:29 +0200

    Bootstrap fix

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from sqlalchemy.sql import func
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask.app import Flask
+import appsettings
 #app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 #db = SQLAlchemy(app)
@@ -10,6 +11,7 @@ from flask.app import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.secret_key = appsettings.secretKey
 db = SQLAlchemy(app)
 
-from routes import register, login, index
+from routes import register, login, index, home
