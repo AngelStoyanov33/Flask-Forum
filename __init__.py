110aa1ec0bd93d10b30a77e9812c0f337708576b
Denis Zahariev <denis.zaharievv@gmail.com>
2021-01-19 19:25:06 +0200

    Updated run.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+from sqlalchemy.sql import func
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
 from flask.app import Flask
+#app = Flask(__name__)
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+#db = SQLAlchemy(app)
 
+    
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
+
+from routes import register, login, index
