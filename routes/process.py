f4b5116d9f02f90a2d904a79fb515cae1b7c940e
AngelStoyanov33 <aststoyanov@gmail.com>
2021-01-24 13:14:40 +0200

    Feature #4 Profile page and edit profile options added

---
+++ routes/process.py
@@ -0,0 +1,39 @@
+from flask import request, render_template, url_for, redirect
+from models.User import *
+from __init__ import app
+from services.AccountRepository import AccountManager as account
+from DTOs.UserDTO import UserDTO
+import json
+from flask.json import jsonify
+from services.access import require_access, ACCESS_LEVELS
+import os
+
+@app.route('/process', methods=['POST', 'GET'])
+def process_data():
+    if request.method == 'POST':
+        token = request.form['token']
+        user = account.get_user_by_token(token)
+
+        username = user.username
+
+        user = User.query.filter_by(username=username).first()
+
+        first_name = request.form['first_name']
+        if first_name != "":
+            user.firstName = first_name
+        last_name = request.form['last_name']
+        if last_name != "":
+            user.lastName = last_name
+        email = request.form['email']
+        if email != "":
+            user.email = email
+        password = request.form['password']
+        if password != "":
+            user.password = account.hashPassword(password)
+
+        db.session.commit()
+        #TODO: password2 and password_conf
+
+        
+
+        return jsonify({'token': ""})
---
+++ routes/profile.py
@@ -0,0 +1,37 @@
+from flask import request, render_template, url_for, redirect
+from models.User import *
+from __init__ import app
+from services.AccountRepository import AccountManager as account
+from DTOs.UserDTO import UserDTO
+import json
+from flask.json import jsonify
+from services.access import require_access, ACCESS_LEVELS
+import os
+
+@app.route('/u/<username>', methods=['POST', 'GET'])
+# All access levels possible
+def profile_info(username):
+    user = User.query.filter_by(username=username).first()
+    isRequestOwner = False
+    if request.method == 'POST':
+        token = request.form['token']
+        isRequestOwner = account.get_user_by_token(token).username == username
+
+    return render_template('profile.html', user = user, isRequestOwner = isRequestOwner)
+
+
+@app.route('/u/<username>/edit', methods=['POST', 'GET'])
+@require_access(ACCESS_LEVELS['logged'])
+def edit_profile_info(username):
+    user = User.query.filter_by(username=username).first()
+    isRequestOwner = False
+    if request.method == 'POST':
+        if len(request.form) > 1:
+            first_name = request.form['first_name']
+            print(first_name)
+        token = request.form['token']
+        isRequestOwner = account.get_user_by_token(token).username == username
+        return render_template('edit_profile.html', user = user)
+    if isRequestOwner == False:
+        return redirect('/home')
+    
