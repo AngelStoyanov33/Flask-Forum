4decd675e2fa2c9705a6c0f035253c886bde478f
AngelStoyanov33 <aststoyanov@gmail.com>
2021-01-26 20:31:11 +0200

    Added allowed file extensions

--- appsettings.py
+++ appsettings.py
@@ -1,2 +1,4 @@
 expire_time = 60000
 secret_key = "d3n1_1-4cho_pr4vqt-f0rym_s_fl4$k?]%Z)m_BBX<i"
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'apng', 'png', 'jpg', 'jpeg', 'gif', 'svg', 'avif', 'jfif'}
